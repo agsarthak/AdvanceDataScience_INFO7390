442cd9ec9bdcbe102ee2f95a65a3b9fc4c203bbd
agsarthak <agarwal.sar@husky.neu.edu>
2017-02-27 17:00:04 -0500

    update

--- Assignment1/Problem2.py
+++ Assignment1/Problem2.py
@@ -7,9 +7,13 @@ import zipfile
 import os
 import pandas as pd
 import logging # for logging
-import sys
 import shutil #to delete the directory contents
 import glob
+import boto.s3
+import sys
+from boto.s3.key import Key
+import time
+import datetime
 
 ############### Initializing logging file ###############
 root = logging.getLogger()
@@ -43,8 +47,7 @@ try:
         os.makedirs('downloaded_zips_unzipped', mode=0o777)
     logging.info('Directories cleanup complete.')
 except Exception as e:
-    logging.error('Error occurred while cleaning up directories. Check console.')
-    print(str(e))
+    logging.error(str(e))
     exit()     
     
 ############### Function to Download zips ###############
@@ -60,15 +63,66 @@ def download_zip(url):
             logging.info('Log file %s successfully downloaded', url[-15:])
             return True
     except Exception as e: #Catching file not found
-        #print(str(e))
         logging.warning('Log %s not found...Skipping ahead!', url[-15:])
         return True
 
+############## Fetch all the command line arguments ###################
+argLen=len(sys.argv)
+year=''
+accessKey=''
+secretAccessKey=''
+inputLocation=''
+
+for i in range(1,argLen):
+    val=sys.argv[i]
+    if val.startswith('year='):
+        pos=val.index("=")
+        year=val[pos+1:len(val)]
+        continue
+    elif val.startswith('accessKey='):
+        pos=val.index("=")
+        accessKey=val[pos+1:len(val)]
+        continue
+    elif val.startswith('secretKey='):
+        pos=val.index("=")
+        secretAccessKey=val[pos+1:len(val)]
+        continue
+    elif val.startswith('location='):
+        pos=val.index("=")
+        inputLocation=val[pos+1:len(val)]
+        continue
+
+print("Year=",year)
+print("Access Key=",accessKey)
+print("Secret Access Key=",secretAccessKey)
+print("Location=",inputLocation)
+
+############### Validate amazon keys ###############
+if not accessKey or not secretAccessKey:
+    logging.warning('Access Key and Secret Access Key not provided!!')
+    print('Access Key and Secret Access Key not provided!!')
+    exit()
+
+AWS_ACCESS_KEY_ID = accessKey
+AWS_SECRET_ACCESS_KEY = secretAccessKey
+
+try:
+    conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
+            AWS_SECRET_ACCESS_KEY)
+
+    print("Connected to S3")
+
+except:
+    logging.info("Amazon keys are invalid!!")
+    print("Amazon keys are invalid!!")
+    exit()
+
+
+
 ############### Generate URLs and download zip for the inputted year ###############
-#http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2016/Qtr1/log20160101.zip
+
 url_pre = "http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/"
 qtr_months = {'Qtr1':['01','02','03'], 'Qtr2':['04','05','06'], 'Qtr3':['07','08','09'], 'Qtr4':['10','11','12']}
-year = input("Please enter the year for which you want to analyze the logs:")
 valid_years = range(2003,2017)
 days = range(1,32)
 
@@ -112,17 +166,35 @@ except Exception as e:
 try:
     filelists = glob.glob('downloaded_zips_unzipped' + "/*.csv")
     all_csv_df_dict = {period: pd.read_csv(period) for period in filelists}
-    logging.info('All the csv read into individual daaframes')
+    logging.info('All the csv read into individual dataframes')
 except Exception as e:
-    logging.error('Error occurred while loading csv to dataframe. Check console for more info.')
-    print(str(e))
+    logging.error(str(e))
     exit()
                    
                    
-############### HANDLING MISSING VALUES for one dataframe at a time ###############
+############### The following section deals with DETECTING ANOMALIES, ###############
+############### HANDLING MISSING VALUES and computing ###############################
+############### SUMMARY METRICS for one dataframe at a time #########################
+
 try:
     for key, val in all_csv_df_dict.items():
         df = all_csv_df_dict[key]
+        #detecting null values
+        null_count = df.isnull().sum()
+        logging.info('Count of Null values for %s in all the variables:\n%s ', key, null_count)
+        
+        # variable idx should be either 0 or 1
+        incorrect_idx = (~df['idx'].isin([0.0,1.0])).sum()
+        logging.info('There are %s idx which are not 0 or 1 in the log file %s', incorrect_idx, key) 
+        
+        # variable norefer should be either 0 or 1
+        incorrect_norefer = (~df['norefer'].isin([0.0,1.0])).sum()
+        logging.info('There are %s norefer which are not 0 or 1 in the log file %s', incorrect_norefer, key) 
+        
+        # variable noagent should be either 0 or 1
+        incorrect_noagent = (~df['noagent'].isin([0.0,1.0])).sum()
+        logging.info('There are %s noagent which are not 0 or 1 in the log file %s', incorrect_noagent, key) 
+        
         #remove rows which have no ip, date, time, cik or accession
         df.dropna(subset=['cik'])
         df.dropna(subset=['accession'])
@@ -152,10 +224,8 @@ try:
         max_find = pd.DataFrame(df.groupby('find').size().rename('cnt')).idxmax()[0]
         df['find'] = df['find'].fillna(max_find)
         
-        
         # replace nan crawler with zero
         df['crawler'] = df['crawler'].fillna('0')
-        
         
         # replace nan extention with max extention
         max_extention = pd.DataFrame(df.groupby('extention').size().rename('cnt')).idxmax()[0]
@@ -168,11 +238,18 @@ try:
         # find mean of the size and replace null values with the mean
         df['size'] = df['size'].fillna(df['size'].mean(axis=0))
         
+        ##### Summary Metrics #####
         #Compute mean size
         df['size_mean'] = df['size'].mean(axis=0)
         
         #Compute maximum used browser
         df['max_browser'] = pd.DataFrame(df.groupby('browser').size().rename('cnt')).idxmax()[0]
+        
+        #Compute distinct count of ip per month i.e. per log file
+        df['ip_count'] = df['ip'].nunique()
+        
+        #Compute distinct count of cik per month i.e. per log file
+        df['cik_count'] = df['cik'].nunique()
     
     logging.info('Rows removed where ip, date, time, cik or accession were null.')
     logging.info('NaN values in browser replaced with maximum count browser.')
@@ -187,9 +264,10 @@ try:
     logging.info('NaN values in size replaced with mean value of size.')
     logging.info('New column added to dataframe: Mean of size.')
     logging.info('New column added to dataframe: Max count of browser.')
+    logging.info('New column added to dataframe: Count of distinct ip per month.')
+    logging.info('New column added to dataframe: Count of distinct cik per month.')
 except Exception as e:
-    logging.error("Error occurred while handling missing data. Check console for more info.")
-    print(str(e))
+    logging.error(str(e))
     exit()
     
 ############### Combining all dataframe and computing overall summary metric ###############
@@ -199,8 +277,7 @@ try:
     master_df.to_csv('master_csv.csv')
     logging.info('All dataframes of csvs are combined and exported as csv: master_csv.csv.')
 except Exception as e:
-    logging.error('Error occurred while exporting merged csv. Check console for more info.')
-    print(str(e))
+    logging.error(str(e))
     exit()
     
 # write csv for summary of combined data.
@@ -209,7 +286,66 @@ try:
     master_df_summary.to_csv('master_df_summary.csv')
     logging.info('The summary metric of combined csv is generated and exported as csv: master_df_summary.csv .')
 except Exception as e:
-    logging.error('Error occurred while exporting summary statistics for merged csv. Check console for more info.')
-    print(str(e))
+    logging.error(str(e))
     exit()
+
+############### Zip the csvs and logs ###############
+def zipdir(path, ziph):
+    ziph.write(os.path.join('master_csv.csv'))
+    ziph.write(os.path.join('master_df_summary.csv'))
+    ziph.write(os.path.join('problem2_log.log'))   
+
+zipf = zipfile.ZipFile('Problem2.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('/', zipf)
+zipf.close()
+logging.info('Compiled csv and log file zipped')
     
+############### Upload the zip to AWS S3 ###############
+############### Fetch the location argument if provided, else user's system location is taken ############### 
+loc=''
+
+if inputLocation == 'APNortheast':
+    loc=boto.s3.connection.Location.APNortheast
+elif inputLocation == 'APSoutheast':
+    loc=boto.s3.connection.Location.APSoutheast
+elif inputLocation == 'APSoutheast2':
+    loc=boto.s3.connection.Location.APSoutheast2
+elif inputLocation == 'CNNorth1':
+    loc=boto.s3.connection.Location.CNNorth1
+elif inputLocation == 'EUCentral1':
+    loc=boto.s3.connection.Location.EUCentral1
+elif inputLocation == 'EU':
+    loc=boto.s3.connection.Location.EU
+elif inputLocation == 'SAEast':
+    loc=boto.s3.connection.Location.SAEast
+elif inputLocation == 'USWest':
+    loc=boto.s3.connection.Location.USWest
+elif inputLocation == 'USWest2':
+    loc=boto.s3.connection.Location.USWest2
+try:   
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts)    
+    bucket_name = AWS_ACCESS_KEY_ID.lower()+str(st).replace(" ", "").replace("-", "").replace(":","").replace(".","")
+    bucket = conn.create_bucket(bucket_name, location=loc)
+    print("bucket created")
+    zipfile = 'Problem2.zip'
+    print ("Uploading %s to Amazon S3 bucket %s", zipfile, bucket_name)
+    
+    def percent_cb(complete, total):
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    
+    k = Key(bucket)
+    k.key = 'Problem2'
+    k.set_contents_from_filename(zipfile,
+        cb=percent_cb, num_cb=10)
+    print("Zip File successfully uploaded to S3")
+except:
+    logging.info("Amazon keys are invalid!!")
+    print("Amazon keys are invalid!!")
+    exit()
+
+################Tableau Public URL###################
+logging.info('Data Visualizations for the year 2003 can be viewed at the following URL')
+logging.info('https://public.tableau.com/profile/publish/ADSTeam9Assignment1/Dashboard1')
+############ EOF ############
